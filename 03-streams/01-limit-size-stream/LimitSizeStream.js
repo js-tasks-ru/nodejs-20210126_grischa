2883b8f88c2cf86aaa6d400c3623e75c9a3899b4
grischaLib <grigori.liberchuk@fashion-digital.de>
2021-01-28 19:43:43 +0100

    TASK-03-01:resolved

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.prceededDataLength = 0;
+    this.limit = options.limit || 8;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const buffer = new Buffer(chunk);
+    this.prceededDataLength = this.prceededDataLength + buffer.byteLength;
+    if (this.prceededDataLength > this.limit) {
+      return callback(new LimitExceededError());
+    }
+    this.push(chunk);
+    callback();
   }
 }
 
