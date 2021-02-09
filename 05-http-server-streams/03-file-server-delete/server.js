e855f7ef0529aa5a9c68caf88e37bfde8549e9c5
grischaLib <grigori.liberchuk@fashion-digital.de>
2021-02-09 17:15:42 +0100

    TASK-05-03: resolved

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,17 +1,30 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
+      if (pathname.indexOf(path.sep) !== -1) {
+        res.statusCode = 400;
+        res.end('Nested path error');
+        return;
+      }
+      if (fs.existsSync(filepath) === false) {
+        res.statusCode = 404;
+        res.end(`File ${filepath} nor found.`);
+        return;
+      }
+      fs.unlink(filepath, () => {
+        res.statusCode = 200;
+        res.end('Ok');
+      });
       break;
 
     default:
