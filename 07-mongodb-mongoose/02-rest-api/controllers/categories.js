1668ec53072b11e36c79b0ca1e8c47babf7facbc
grischaLib <grigori.liberchuk@fashion-digital.de>
2021-02-22 19:34:40 +0100

    TASK-07-02: resolved

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,22 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({}).populate('Category.subcategories');
+  const categoriesDto = [];
+  categories.forEach((category) => {
+    const dto = {
+      id: category._id,
+      title: category.title,
+      subcategories: [],
+    };
+    category.subcategories.forEach((subcategorie) => {
+      dto.subcategories.push({
+        id: subcategorie._id,
+        title: subcategorie.title,
+      });
+    });
+    categoriesDto.push(dto);
+  });
+  ctx.body = {categories: categoriesDto};
+  return next();
 };
